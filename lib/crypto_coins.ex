ccda5f75ad03ab9e51e460a6e258b350f1e8cfcd
Pedro Mendoza <lpedromendoza333@gmail.com>
2019-11-30 00:53:23 -0600

    Funciones termindas

--- lib/crypto_coins.ex
+++ lib/crypto_coins.ex
@@ -1,16 +1,40 @@
 defmodule CryptoCoins do
+
+  use Tesla
+
+    plug Tesla.Middleware.BaseUrl, "https://api.coinranking.com/v1/public"
+    plug Tesla.Middleware.Headers, [{"", ""}]
+    plug Tesla.Middleware.JSON
+
+  require CryptoCoins.Api
+
   def coin(coins, name) when is_binary(name) do
+    # Con Enum.find se busca mediante la key name de cada coin del array coins que sea igual
+    # al name pasado como parámetro
+    Enum.find(coins, fn coin -> String.downcase(Map.fetch!(coin, "name")) == String.downcase(name) end)
   end
 
   def most_valuable(coins) do
+    # Obtiene la moneda con el precio más alto del array coins donde se encuetra cada coin, comparando con el precio
+    # de cada coin convertido a float.
+    Enum.max_by(coins, fn coin -> Float.parse(Map.fetch!(coin, "price")) end)
   end
 
   def most_capped(coins) do
+    # Obtiene la moneda con el precio más najo del array coins donde se encuetra cada coin, comparando con el precio
+    # de cada coin convertido a float.
+    Enum.min_by(coins, fn coin -> Float.parse(Map.fetch!(coin, "price")) end)
   end
 
   def most_ranked(coins) do
+    # Obtiene la moneda con el rank menor del array coins donde se encuetra cada coin, comparando con el rank
+    # de cada coin convertido a Intenger, se obtiene el menor ya que el rank más alto se considera el 1.
+    Enum.min_by(coins, fn coin -> Map.fetch!(coin, "rank") end)
   end
 
   def sort_by(coins, field) do
+    # Mediante sort_by se ordena cada coin en el array coins, obteniendo la key pasado como parametro
+    # con field.
+    Enum.sort_by(coins, fn coin -> Map.fetch!(coin, field) end)
   end
 end
--- lib/crypto_coins/api.ex
+++ lib/crypto_coins/api.ex
@@ -1,14 +1,33 @@
-defmodule Api.CryptoCoins do
+defmodule CryptoCoins.Api do
   # Check Tesla Docs
   # API Base is: "https://api.coinranking.com/v1/public"
   # Endpoint is: /coins
 
   # returns {:ok, array_of_coins}
+
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://api.coinranking.com/v1/public"
+  plug Tesla.Middleware.Headers, [{"", ""}]
+  plug Tesla.Middleware.JSON
+
+
   def coins() do
+    {:ok, response} = get("/coins")
+    if(response.status in 200..299) do
+      coins = response |> Map.fetch!(:body) |> Map.fetch!("data") |> Map.fetch!("coins")
+    end
   end
 
   # returns array_of_coins
   #raise if error
   def coins!() do
+    {:ok, response} = get("/coins")
+    if(response.status in 200..299) do
+      coins = response |> Map.fetch!(:body) |> Map.fetch!("data") |> Map.fetch!("coins")
+    else
+      raise "No sirvio"
+    end
   end
+
 end
